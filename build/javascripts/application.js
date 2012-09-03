0d806fbe7b60e5d8df777803b5d1eb9e851ae71c
Shane Riley <shane@shaneriley.com>
2012-09-03 06:00:40 -0400

    Sausages!

---
+++ build/javascripts/application.js
@@ -0,0 +1,37 @@
+if (!("addEventListener" in document)) {
+  document.ready = function(cb) { document.attachEvent("onload", cb); };
+}
+else {
+  document.ready = function(cb) { document.addEventListener("DOMContentLoaded", cb, false); };
+}
+
+document.ready(function() {
+  var img = document.createElement("img"),
+      a = document.createElement("audio"),
+      src = document.createElement("source"),
+      ctx = document.createElement("canvas").getContext("2d"),
+      m = document.createElement("canvas").getContext("2d"),
+      w = 240, h, frames, frame = 0;
+  var draw = function() {
+    ctx.clearRect(0, 0, w, h);
+    ctx.drawImage(img, -(frame * w), 0);
+    if (++frame < frames) {
+      setTimeout(draw, 1000 / 24);
+    }
+    else {
+      ctx.canvas.parentElement.removeChild(ctx.canvas);
+    }
+  };
+
+  a.setAttribute("autoplay", true);
+  a.src = "audio/sausages.mp3";
+  img.src = "images/sausages.png";
+  img.onload = function() {
+    ctx.canvas.width = w;
+    ctx.canvas.height = h = img.height;
+    frames = img.width / w;
+    draw();
+  };
+  document.body.appendChild(ctx.canvas);
+  ctx.canvas.style.cssText = "position: fixed; left: " + ((window.innerWidth - w) / 2) + "px; top: 0; z-index: 10000000;"
+});
